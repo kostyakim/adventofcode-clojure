84e29c8f349db1433fa8a945925d4be786e60e11
Kumiho <konstantin.kim.mail@gmail.com>
2017-12-12 23:08:21 +0300

    move day 1 task

---
+++ src/adventofcode_clojure/day1.clj
@@ -0,0 +1,18 @@
+(ns adventofcode-clojure.core
+  (:gen-class))
+
+(def inputString (slurp "/home/kuma/first"))
+(defn numCount 
+  [inp pos sum]
+  (letfn [(parse-int [s](Integer. s))
+          (subNumber [n p] (parse-int (subs n p (+ p 1))))
+          (sum-equal [n p](= (subNumber n p) (subNumber n (+ p 1))))
+          (get-new-sum [n p sum](if (sum-equal n p) (+ sum (subNumber n p)) sum))
+          ]
+  (if (= (+ pos 2) (count inp))
+    sum
+    (recur inp (+ 1 pos) (get-new-sum inp pos sum))
+  ))
+)
+
+
